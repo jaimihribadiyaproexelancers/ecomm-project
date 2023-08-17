8769ff21b0fef151837cb4c528b7c5d6a2a45028
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-17 17:49:58 +0530

    - Create interface and add signup flow interface

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SellerService } from '../services/seller.service';
 import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
@@ -10,7 +11,7 @@ import { Router } from '@angular/router';
 export class SellerAuthComponent {
   constructor(private seller: SellerService, private router: Router) {}
   ngOnInit(): void {}
-  signUp(data: object): void {
+  signUp(data: SignUp): void {
     this.seller.userSignUp(data).subscribe((result) => {
       if (result) {
         this.router.navigate(['seller-home']);
