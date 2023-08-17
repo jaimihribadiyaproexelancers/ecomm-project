0bcdcbbca9f36886488ed8c3315ee7cd0dbb972b
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-17 16:36:18 +0530

    -- Create seller signup form and method

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
-  styleUrls: ['./seller-auth.component.css']
+  styleUrls: ['./seller-auth.component.css'],
 })
 export class SellerAuthComponent {
-
+  signUp(data: object): void {
+    console.warn(data);
+  }
 }
