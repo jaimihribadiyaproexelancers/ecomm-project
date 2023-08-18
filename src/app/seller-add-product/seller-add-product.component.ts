5e198319c669fcb4ec8e5595d8ebc52a61d50d69
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-18 14:47:29 +0530

    -- Create add product form

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.css']
+  styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent {
+  constructor() {}
+  ngOnInit(): void {}
 
+  submit(data: object) {
+    console.warn(data);
+  }
 }
