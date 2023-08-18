49a5101fec5dfd1c8bec5b033c82531c039e8a49
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-18 16:49:45 +0530

    - Create update product and set routing

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../data-type';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash,faFilePen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
@@ -12,6 +12,7 @@ export class SellerHomeComponent {
   productList: undefined | Product[];
   productMessage: undefined | string;
   iconTr=faTrash;
+  iconUp=faFilePen;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
