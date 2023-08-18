f1c3c028b37d43b5268401f8a262e00f2fa1fcb8
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-18 12:42:09 +0530

    - Set nav menu on header when seller login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  menuType: String = 'default';
+  constructor(private route: Router) {}
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        console.warn(val.url);
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
