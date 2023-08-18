f587b5cbdb21acfaddc81286c273eb4b010d9776
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-18 15:25:18 +0530

    - Create add product form and set design - Create add product api

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -3,7 +3,15 @@ export interface SignUp {
   email: string;
   password: string;
 }
-export interface Login {  
+export interface Login {
   email: string;
   password: string;
+}
+export interface Product {
+  name: string;
+  price: number;
+  category: string;
+  color: string;
+  description: string;
+  image: string;
 }
--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,10 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent {
-  constructor() {}
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
   ngOnInit(): void {}
 
-  submit(data: object) {
-    console.warn(data);
+  submit(data: Product): void {
+    this.product.addProduct(data).subscribe((result) => {
+      if (result) {
+        this.addProductMessage = 'Product has been added';
+      }
+      setTimeout(() => (this.addProductMessage = undefined), 3000);
+    });
   }
 }
