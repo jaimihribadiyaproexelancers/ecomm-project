ff66bf08aa85c5fb32e8b1f4557c3b64c99a32f8
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-21 15:04:48 +0530

    - Create user signup module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './search/search.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     SearchComponent,
     FooterComponent,
     ProductDetailsComponent,
+    UserAuthComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, FontAwesomeModule, NgbModule],
   providers: [],
---
+++ src/app/user-auth/user-auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SignUp } from '../data-type';
+
+@Component({
+  selector: 'app-user-auth',
+  templateUrl: './user-auth.component.html',
+  styleUrls: ['./user-auth.component.css'],
+})
+export class UserAuthComponent {
+  constructor() {}
+  ngOnInit(): void {}
+  signUp(data: SignUp) {}
+}
