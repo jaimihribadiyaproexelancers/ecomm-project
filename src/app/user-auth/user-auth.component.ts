c5e120aa7cc8a1e2073740fd54a918b928ca7efd
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-21 16:32:45 +0530

    - Set login user method

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -8,14 +8,24 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent {
-  showLogin = true;
+  showLogin: boolean = true;
+  authError: string = '';
   constructor(private user: UserService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user.userAuthReload();
+  }
   signUp(data: SignUp) {
     this.user.userSignUp(data);
   }
   login(data: Login) {
-    
+    this.user.userLogin(data);
+    this.user.invalidUserAuth.subscribe((result) => {
+      console.warn(result);
+      if (result) {
+        this.authError = 'User not found';
+      } else {
+      }
+    });
   }
   openLogin() {
     this.showLogin = true;
