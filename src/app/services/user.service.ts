3c53054d0a5e736f3919c4cd1277c8661bc2ed0d
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-21 15:31:45 +0530

    - Create user service for user - Create signup form and method

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { SignUp } from '../data-type';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient, private router: Router) {}
+  userSignUp(user: SignUp) {
+    this.http
+      .post(`http://localhost:3000/user`, user, {
+        observe: 'response',
+      })
+      .subscribe((result) => {
+        localStorage.setItem('user', JSON.stringify(result.body));
+        this.router.navigate(['/']);
+      });
+  }
+}
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SignUp } from '../data-type';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-auth',
@@ -7,7 +8,9 @@ import { SignUp } from '../data-type';
   styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent {
-  constructor() {}
+  constructor(private user: UserService) {}
   ngOnInit(): void {}
-  signUp(data: SignUp) {}
+  signUp(data: SignUp) {
+    this.user.userSignUp(data);
+  }
 }
