5538888d52cb514341e5991388daba39bdb4de7d
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-22 15:39:16 +0530

    --

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -87,11 +87,20 @@ export class ProductService {
   orderNow(data: Order) {
     return this.http.post('http://localhost:3000/orders', data);
   }
-  // deleteCartItems(cartId: string) {
-  //   return this.http
-  //     .delete(`http://localhost:3000/cart/${cartId}`)
-  //     .subscribe((result) => {
-  //       this.cartData.emit([]);
-  //     });
+  // orderList() {
+  //   let userStore = localStorage.getItem('user');
+  //   let userData = userStore && JSON.parse(userStore);
+  //   return this.http.get<order[]>('http://localhost:3000/orders?userId=' + userData.id);
+  // }
+
+  // deleteCartItems(cartId: number) {
+  //   return this.http.delete('http://localhost:3000/cart/' + cartId).subscribe((result) => {
+  //     this.cartData.emit([]);
+  //   })
+  // }
+
+  // cancelOrder(orderId:number){
+  //   return this.http.delete('http://localhost:3000/orders/'+orderId)
+
   // }
 }
