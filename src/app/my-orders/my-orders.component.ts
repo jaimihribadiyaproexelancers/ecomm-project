4e64bf4fa174c3a9d29afdd49f5efd04e9aec9fe
Jaimish Proexelancers <jaimish.ribadiya@proexelancers.com>
2023-08-22 16:50:18 +0530

    - Cancel order method

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -11,6 +11,17 @@ export class MyOrdersComponent {
   orderData: Order[] | undefined;
   constructor(private product: ProductService) {}
   ngOnInit(): void {
+    this.getOrderList();
+  }
+  cancelOrder(orderId: number | undefined) {
+    orderId &&
+      this.product.cancelOrder(orderId).subscribe((result) => {
+        if (result) {
+          this.getOrderList();
+        }
+      });
+  }
+  getOrderList() {
     this.product.orderList().subscribe((result) => {
       this.orderData = result;
     });
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -103,8 +103,7 @@ export class ProductService {
       });
   }
 
-  // cancelOrder(orderId:number){
-  //   return this.http.delete('http://localhost:3000/orders/'+orderId)
-
-  // }
+  cancelOrder(orderId: number) {
+    return this.http.delete(`http://localhost:3000/orders/${orderId}`);
+  }
 }
